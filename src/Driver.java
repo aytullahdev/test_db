71774f7aaa10a9f0c405cfd0eaacb73a76803b40
unknown <ayt.ullah.dev@gmail.com>
2021-11-25 23:15:22 +0600

    adding project files

---
+++ src/Driver.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Driver {
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement smt = null;
+        try{
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:user.db");
+            con.setAutoCommit(false);
+            System.out.println("Database Open");
+            smt = con.createStatement();
+            String sql = "DELETE FROM user_information WHERE email = 'opu@gmil.com' ";
+            smt.executeUpdate(sql);
+            smt.close();
+            con.commit();
+            con.close();
+        }catch(Exception e){
+            System.out.println("Error");
+        }
+    }
+}
